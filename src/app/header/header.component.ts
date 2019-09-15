e6b38f0669b0c332a83a2152ce78abdd4082561d
Laurel Thomson <laurel.beth.thomson@gmail.com>
2019-09-15 17:53:33 -0500

    Making mobile nav bar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  mobileNavOpen: boolean; 
   constructor() { }
-
   ngOnInit() {
+  }
+
+  public openMobileNav() {
+    this.mobileNavOpen = !this.mobileNavOpen;
   }
 
 }
