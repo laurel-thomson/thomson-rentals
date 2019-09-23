1f395720102f585f4cee68ddc8b025cd865afe16
Laurel Thomson <laurel.beth.thomson@gmail.com>
2019-09-22 20:04:17 -0500

    Adding an ApartmentsComponents and implementing routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ApartmentsComponent } from './apartments/apartments.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'apartments', component: ApartmentsComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
