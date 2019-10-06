92f0f6c1893b50f0c67f2a03427af93bace217e1
Laurel Thomson <laurel.beth.thomson@gmail.com>
2019-10-06 18:56:42 -0500

    Attempting to send a test email, not working

--- server/contact.controller.js
+++ server/contact.controller.js
@@ -1,5 +1,24 @@
+const nodemailer = require('nodemailer');
+
 exports.email = (req, res) => {
-    //todo: send email
     console.log('SENDING EMAIL....');
+    let transport = nodemailer.createTransport({
+        host: 'laurel228@hotmail.com',
+        port: 2525,
+        auth: {
+            user: 'laurel228@hotmail.com',
+            pass: 'Lmlen!_702'
+        }
+    });
+    const message = {
+        from: 'laurel228@hotmail.com',
+        to: 'laurel228@hotmail.com',
+        subject: 'TESTING',
+        text: 'This is a test email'
+    };
+    transport.sendMail(message, function(err, info) {
+        if (err) console.log(err);
+        else console.log(info);
+    });
     res.send('email sent');
 }
