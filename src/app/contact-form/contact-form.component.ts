88f440bc12654ef6e911bec42261295d96b52f38
laurel-thomson <laurel.beth.thomson@gmail.com>
2020-03-15 11:43:41 -0700

    Adjusting styling

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -8,13 +8,14 @@ import { ApiService } from '../../api.service';
   styleUrls: ['./contact-form.component.scss']
 })
 export class ContactFormComponent implements OnInit {
-
   constructor(private apiService : ApiService) { }
+  sendingEmail: boolean;
 
   ngOnInit() {
   }
 
   onSubmit(f: NgForm) {
+    this.sendingEmail = true;
     this.apiService.sendEmail(f.form.value).subscribe(
       (res) => this.onSuccess(res),
       (err) => this.onError(err)
@@ -22,10 +23,12 @@ export class ContactFormComponent implements OnInit {
   }
 
   onSuccess = (res) => {
+    this.sendingEmail = false;
     console.log(JSON.stringify(res));
   }
 
   onError = (err) => {
+    this.sendingEmail = false;
     console.log(JSON.stringify(err));
   }
 
